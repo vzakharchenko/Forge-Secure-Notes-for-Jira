2670ce946b20f32ee4ce2db69b4889dab5dcc62d
vzakharchenko <vaszakharchenko@gmail.com>
2025-11-15 14:20:09 +0200

    added rovo:agent

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import FiveMinuteTrigger from "./controllers/triggers/FiveMinutesTrigger";
 import SlowQueryTriggerTrigger from "./controllers/triggers/SlowQueryTriggerTrigger";
 import DropSchemaMigrationTrigger from "./controllers/triggers/DropSchemaMigrationTrigger";
 import ApplySchemaMigrationTrigger from "./controllers/triggers/ApplySchemaMigrationTrigger";
+import { ROVO_SERVICE } from "./core/services/RovoService";
 
 const issueResolver = new Resolver();
 const globalResolver = new Resolver();
@@ -27,3 +28,5 @@ export const dropMigrations = DropSchemaMigrationTrigger.handler;
 export const fetchMigrations = async () => {
   return fetchSchemaWebTrigger();
 };
+
+export const runSecurityNotesQuery = ROVO_SERVICE.runSecurityNotesQuery;
