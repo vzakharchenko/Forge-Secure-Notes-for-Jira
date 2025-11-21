29340c71ef40a463eaef314ddd037d3a36bd5471
vzakharchenko <vaszakharchenko@gmail.com>
2025-11-21 15:21:12 +0200

    fix audit

--- static/knip.config.ts
+++ static/knip.config.ts
@@ -4,7 +4,13 @@ import { defaultConfig } from "../knip.config";
 const config: KnipConfig = {
   ...defaultConfig,
   project: ["src/**"],
-  ignoreDependencies: ["@atlaskit/css-reset", "@types/react-router-dom", "history"],
+  ignoreDependencies: [
+    "@atlaskit/css-reset",
+    "@types/react-router-dom",
+    "history",
+    "@src",
+    "@shared",
+  ],
 };
 
 export default config;
