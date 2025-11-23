c3be462ae8c939eae6f9ee150a8273822834f5cf
vzakharchenko <vaszakharchenko@gmail.com>
2025-11-23 11:16:25 +0200

    added analytics

--- src/controllers/triggers/FiveMinutesTrigger.ts
+++ src/controllers/triggers/FiveMinutesTrigger.ts
@@ -1,15 +1,46 @@
 import { SECURITY_NOTE_SERVICE } from "../../core/services/SecurityNoteService";
 import { schedulerTrigger } from "../../core/decorators/SchedulerDecorator";
-import { SchedulerTrigger, SchedulerTriggerResponse } from "../../core/trigger/SchedulerTrigger";
+import {
+  SchedulerTrigger,
+  SchedulerTriggerRequest,
+  SchedulerTriggerResponse,
+} from "../../core/trigger/SchedulerTrigger";
 import { exceptionHandlerTrigger } from "../../core/decorators/ExceptionHandlerDecorator";
 import { clearCacheSchedulerTrigger } from "forge-sql-orm";
+import { FORGE_SQL_ORM } from "../../database/DbUtils";
+import { ANALYTIC_SERVICE } from "../../core/services/AnalyticService";
 
 @schedulerTrigger
 class FiveMinutesTrigger implements SchedulerTrigger {
   @exceptionHandlerTrigger("Five Minutes Trigger Error")
-  async handler(): Promise<SchedulerTriggerResponse<string>> {
-    await SECURITY_NOTE_SERVICE.expireSecurityNotes();
-    return await clearCacheSchedulerTrigger({ cacheEntityName: "cache", logRawSqlQuery: true });
+  async handler(request: SchedulerTriggerRequest): Promise<SchedulerTriggerResponse<string>> {
+    return FORGE_SQL_ORM.executeWithMetadata(
+      async () => {
+        await SECURITY_NOTE_SERVICE.expireSecurityNotes();
+        return await clearCacheSchedulerTrigger({ cacheEntityName: "cache", logRawSqlQuery: true });
+      },
+      async (totalDbExecutionTime, totalResponseSize, printQueriesWithPlan) => {
+        const resolverName = request.context.moduleKey;
+        await ANALYTIC_SERVICE.sendAnalytics(
+          "sql_5mins_performance",
+          resolverName,
+          request.context.cloudId,
+          { totalDbExecutionTime, totalResponseSize },
+        );
+        if (totalDbExecutionTime > 2000) {
+          // eslint-disable-next-line no-console
+          console.warn(
+            `Resolver ${resolverName} has high database execution time: ${totalDbExecutionTime}ms`,
+          );
+          await printQueriesWithPlan();
+        } else if (totalDbExecutionTime > 1000) {
+          // eslint-disable-next-line no-console
+          console.debug(
+            `Resolver ${resolverName} has high database execution time: ${totalDbExecutionTime}ms`,
+          );
+        }
+      },
+    );
   }
 }
 
---
+++ src/core/services/AnalyticService.ts
@@ -0,0 +1,51 @@
+import { fetch, getAppContext } from "@forge/api";
+
+export interface AnalyticService {
+  sendAnalytics(
+    eventName: string,
+    resolverName: string,
+    cloudId: string,
+    data: { totalDbExecutionTime: number; totalResponseSize: number },
+  ): Promise<void>;
+}
+
+class AnalyticServiceImpl implements AnalyticService {
+  parseVersion(raw: string) {
+    return raw.replace(/[^0-9]/g, "");
+  }
+  async sendAnalytics(
+    eventName: string,
+    resolverName: string,
+    cloudId: string,
+    data: { totalDbExecutionTime: number; totalResponseSize: number },
+  ): Promise<void> {
+    if (!process.env.ANALYTICS_API_KEY) {
+      return;
+    }
+    const appContext = getAppContext();
+    const properties = {
+      resolverName,
+      cloudId,
+      envName: appContext.environmentType,
+      envId: appContext.environmentAri.environmentId,
+      version: appContext.appVersion,
+      parsedVersion: this.parseVersion(appContext.appVersion),
+      totalDbExecutionTime: data.totalDbExecutionTime,
+      totalResponseSize: data.totalResponseSize,
+      eventVersion: 1,
+    };
+    await fetch("https://eu.i.posthog.com/capture/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        api_key: process.env.ANALYTICS_API_KEY,
+        event: eventName,
+        distinct_id: cloudId,
+        timestamp: new Date().toISOString(),
+        properties: properties,
+      }),
+    });
+  }
+}
+
+export const ANALYTIC_SERVICE: AnalyticService = new AnalyticServiceImpl();
