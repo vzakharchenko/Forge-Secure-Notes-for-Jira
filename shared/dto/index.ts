08b9a15a52b6ed4b5cf320bc2c5ec27d595e329e
vzakharchenko <vaszakharchenko@gmail.com>
2025-12-05 12:06:06 +0200

    added di

---
+++ shared/dto/index.ts
@@ -0,0 +1,6 @@
+export { SecurityNoteId } from "./SecurityNoteId";
+export { NewSecurityNote } from "./NewSecurityNote";
+export { SecurityNoteIdAndSecurityHashKey } from "./SecurityNoteIdAndSecurityHashKey";
+export { IssueIdWithPagination } from "./IssueIdWithPagination";
+export { SecurityAccountId } from "./SecurityAccountId";
+export { ProjectWithPagination } from "./ProjectWithPagination";
---
+++ shared/responses/index.ts
@@ -0,0 +1,8 @@
+export type { AuditUser } from "./AuditUser";
+export type { AuditUsers } from "./AuditUsers";
+export type { Bootstrap } from "./Bootstrap";
+export type { OpenSecurityNote } from "./OpenSecurityNote";
+export type { ProjectInfo, ProjectIssue } from "./ProjectIssue";
+export { PERMISSION_ERROR_OBJECT } from "./SecurityNoteData";
+export type { SecurityNoteData } from "./SecurityNoteData";
+export type { UserViewInfoType, ViewMySecurityNotes } from "./ViewMySecurityNotes";
