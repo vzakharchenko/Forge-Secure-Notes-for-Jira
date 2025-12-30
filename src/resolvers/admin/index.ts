a1bd058381b678b82bcf4293e1f442bbee38c496
vzakharchenko <vaszakharchenko@gmail.com>
2025-12-30 17:45:11 +0200

    fixed rovo

--- src/resolvers/admin/index.ts
+++ src/resolvers/admin/index.ts
@@ -1,5 +1,14 @@
 import Resolver from "@forge/resolver";
-import { AuditUserController, AuditUsersController, BootStrapController } from "../../controllers";
+import {
+  OpenSecurityNoteController,
+  FetchSecurityNoteController,
+  AuditUserController,
+  AuditUsersController,
+  IssueProjectsController,
+  IssueAuditController,
+  ProjectAuditController,
+  BootStrapController,
+} from "../../controllers";
 
 import { FORGE_INJECTION_TOKENS } from "../../constants";
 import { withContainer } from "../../core/decorators";
@@ -18,6 +27,11 @@ const ADMIN_BINDINGS = [
   { name: FORGE_INJECTION_TOKENS.AuditUserController, bind: AuditUserController },
   { name: FORGE_INJECTION_TOKENS.AuditUsersController, bind: AuditUsersController },
   { name: FORGE_INJECTION_TOKENS.BootStrapController, bind: BootStrapController },
+  { name: FORGE_INJECTION_TOKENS.FetchSecurityNoteController, bind: FetchSecurityNoteController },
+  { name: FORGE_INJECTION_TOKENS.IssueAuditController, bind: IssueAuditController },
+  { name: FORGE_INJECTION_TOKENS.IssueProjectsController, bind: IssueProjectsController },
+  { name: FORGE_INJECTION_TOKENS.OpenSecurityNoteController, bind: OpenSecurityNoteController },
+  { name: FORGE_INJECTION_TOKENS.ProjectAuditController, bind: ProjectAuditController },
 
   { name: FORGE_INJECTION_TOKENS.SecurityNoteService, bind: SecurityNoteService },
   { name: FORGE_INJECTION_TOKENS.SecurityNoteRepository, bind: SecurityNoteRepository },
@@ -31,10 +45,25 @@ const ADMIN_BINDINGS = [
 
 export const admin = withContainer(...ADMIN_BINDINGS)((container, resolver: Resolver) => {
   container
+    .get<OpenSecurityNoteController>(FORGE_INJECTION_TOKENS.OpenSecurityNoteController)
+    .register(resolver, container);
+  container
+    .get<FetchSecurityNoteController>(FORGE_INJECTION_TOKENS.FetchSecurityNoteController)
+    .register(resolver, container);
+  container
     .get<AuditUserController>(FORGE_INJECTION_TOKENS.AuditUserController)
     .register(resolver, container);
   container
     .get<AuditUsersController>(FORGE_INJECTION_TOKENS.AuditUsersController)
+    .register(resolver, container);
+  container
+    .get<IssueProjectsController>(FORGE_INJECTION_TOKENS.IssueProjectsController)
+    .register(resolver, container);
+  container
+    .get<IssueAuditController>(FORGE_INJECTION_TOKENS.IssueAuditController)
+    .register(resolver, container);
+  container
+    .get<ProjectAuditController>(FORGE_INJECTION_TOKENS.ProjectAuditController)
     .register(resolver, container);
   container
     .get<BootStrapController>(FORGE_INJECTION_TOKENS.BootStrapController)
