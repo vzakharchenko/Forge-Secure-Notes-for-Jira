ae0afb5bcab2d3392789b5a2c037faf2825e2155
vaszakharchenko@gmail.com <vaszakharchenko@gmail.com>
2026-05-31 10:09:39 +0300

    Update Vitest config to include "scripts" in coverage and unify formatting

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,21 +2,20 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-
     globals: true,
     mockReset: true,
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'lcov'],
-      include: ["src","shared"],
-      reportsDirectory: './coverage',
-        thresholds: {
-            statements: 85,
-            branches: 85,
-            functions: 85,
-            lines: 85,
-        },
+      provider: "v8",
+      reporter: ["text", "lcov"],
+      include: ["src", "scripts", "shared"],
+      reportsDirectory: "./coverage",
+      thresholds: {
+        statements: 85,
+        branches: 85,
+        functions: 85,
+        lines: 85,
+      },
     },
-    include: ['src/**/*.test.ts', '__tests__/**/*.test.ts'],
+    include: ["src/**/*.test.ts", "__tests__/**/*.test.ts"],
   },
 });
